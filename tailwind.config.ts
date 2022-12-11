92b8dd3e525d66deff8bbaa8aebdd1f63d57d83d
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2022-12-11 09:01:51 +0545

    fix: remove redundant content and use correct format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,11 @@
 import type { Config } from 'tailwindcss';
 import headlessui from '@headlessui/tailwindcss';
-import formkit from '@formkit/themes/tailwindcss'
+import formkit from '@formkit/themes/tailwindcss';
+
+const rootDir = process.cwd();
 
 export default <Partial<Config>>{
-  content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-    'app.vue',
-  ],
+  content: [`${rootDir}/nuxt.config.ts`, `${rootDir}/formkit.config.ts`],
   theme: {},
   plugins: [headlessui({}), formkit],
 };
